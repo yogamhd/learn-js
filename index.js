ca8fc9f96308c3741838971352e56bfee0e0695b
yogamhd <yogapratamamhd@gmail.com>
2019-11-05 15:31:16 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,140 @@
+/*console.log('Hello World')
+console.warn('Hello Warning')
+console.error('Hello Error')
+console.info('Hello Info')
+alert('Hello Alert')
+
+
+
+console.log('Hello World')
+const name = 'Yoga Pratama'
+console.log(`Hello ${name}`)
+alert(`Hello ${name}`)
+*/
+
+
+/*var name1 = 'Yoga Pratama M';
+
+console.log(name1);
+
+var human = {
+    name:'Yoga',
+    age:'22'
+}
+console.log(human);
+
+var firstName = 'Yoga';
+var lasttName = 'Pratama';
+
+console.log(`Hallo ${firstName} ${lasttName}`); //ini disarankan
+
+console.log('Hallo ' + firstName + ' ' + lasttName);
+
+var greetings = `
+
+Hallo, Nama saya Yoga
+`;
+
+console.log(greetings);
+
+var humans = {
+    name: 'Yoga Pratama',
+    website: 'testing.com',
+   twitter: 'twitter',
+    github: 'github.com'
+  }
+  
+console.log(humans);
+
+
+//Array
+
+var studens =['Akla', 'Yoga', 'Mega', 'Mei'];
+console.log(studens);
+console.log(studens [3]);
+console.log(studens[studens.length -1]);
+console.log(studens[3]);
+
+var studentMatriks=[
+    ['Akla', 'Yoga'],
+    ['Mei', 'Mega'],
+    ['Yosua', 'Dona', ['Apriadi']]
+];
+console.log(studentMatriks[1]); // output ['Mei'. 'Mega']
+console.log(studentMatriks[2][2][0]); //output: 'Apriadi'
+*/
+
+//Object
+
+/*var manusia = {
+    name: 'Yoga',
+    age: 22,
+    isMarried: false,
+    hobbies: ['eating', 'singing', 'sleeping'],
+    address: {
+        zipcode:'30662',
+        city:'jakarta',
+        street:'Kemang Selatan'
+    }
+};
+
+console.log(manusia.name); //Output: 'Yoga' >Rekomended
+console.log(manusia['name']); //Output: 'Yoga' >tidak rekomendasi karena puyeng
+
+console.log(manusia.hobbies[2]); //Output: 'sleeping'
+console.log(manusia.address.city); //Ouput: 'jakarta'
+*/
+
+//mereassign variabel
+
+/*var manusia = {
+    name: 'Yoga',
+    age: 22,
+    isMarried: false,
+    hobbies: ['eating', 'singing', 'sleeping'],
+    address: {
+        zipcode:'30662',
+        city:'jakarta',
+        street:'Kemang Selatan'
+    }
+}; */
+
+//manusia.name='Budi';
+//console.log(manusia.name); //cara mengubah variabel
+
+
+/*var studens =['Akla', 'Yoga', 'Mega', 'Mei'];
+studens[1]='Apriadi';
+console.log(studens[1]); */
+
+/*var name = 'Yoga';
+name = 'Pratama;';
+console.log(name); //ouput: Pratama
+
+//Array
+var studens =['Akla', 'Yoga', 'Mega', 'Mei'];
+studens[0]='Apriadi';
+console.log('Apriyadi');
+
+//Object
+var human = {
+    name: 'Yoga',
+    age: 17
+};
+human.age = 24;
+console.log(human.age); //output 24
+
+//undefined and null
+var animal;
+console.log(animal);
+//debugger;
+//animal='koceng';
+//console.log(animal);
+
+//var month = null; //belum diisi
+//console.log(null);
+//console.log(month);
+//console.log(month+1); */
+
+
+
