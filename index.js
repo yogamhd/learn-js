7babd91019f8c32cd7f2ec1713f9e22a1f853e36
yogamhd <yogapratamamhd@gmail.com>
2019-11-05 16:14:39 +0700

    Second commit

--- index.js
+++ index.js
@@ -137,4 +137,72 @@ console.log(animal);
 //console.log(month+1); */
 
 
+/*function firstName(Yoga) {
+    console.log(`Hello, ${Yoga}!`)
+  }
+  
+  const nameA = 'Yoga'
+  const nameB = 'Pratama'
+  
+  console.log(nameA)
+  console.log(nameB)
 
+
+  const checkAge = function(age) {
+    if (age >= 18) {
+      console.log('You are old enough')
+    } else if (age < 18 && age >= 0) {
+      console.log('You are still young')
+    } else {
+      console.log('You are not born yet')
+    }
+  }
+  
+  checkAge(10) // You are still young
+  checkAge(25) // You are old enough
+  checkAge(-2) // You are not born yet
+
+
+
+  const addMarks = (text, mark, times) => {
+    let newText = text
+  
+    for (let i = 0; i <= times; i++) {
+      newText += mark
+    }
+  
+    console.log(newText)
+  }
+  
+  addMarks('Hello', '!', 1) // Hello!
+  addMarks('Hello', '!', 3) // Hello!!!
+  addMarks('How are you', '?', 1) // How are you?
+  addMarks('How are you', '?', 2) // How are you??
+  
+  
+
+  const showNameWithAge = (name = 'Unknown', age = 0) => {
+    const nameWithAge = `${name} is ${age} year(s) old`
+  
+    return nameWithAge // return the value to be used later
+  }
+  
+  const alpha = showNameWithAge('Alpha', 11)
+  const betty = showNameWithAge('Betty', 42)
+  const gamma = showNameWithAge('Gamma', 30)
+  
+  console.log(alpha)
+  console.log(betty)
+  console.log(gamma)
+*/
+
+var number1 = prompt('Isi angka pertama')
+var number2 = prompt('Isi angka kedua')
+
+function add(num1, num2){
+    var result = Number(num1) +Number(num2)
+    return result
+}
+
+console.log(add(number1, number2))
+console.log(add(3,7))
